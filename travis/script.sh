364711dc1582886e329b57af760ecafa2524cde3
Peter Wilemsen <peter@codebuffet.co>
2014-07-07 11:34:16 +0200

    Added automatic github release via travis, fingers crossed...

---
+++ travis/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+xctool -project OpenKit.xcodeproj -scheme OpenKit
+python package_sdk.py
+
+if [[ $TRAVIS_BRANCH != 'master' ]]
+	echo "Not on master branch, Deleting Gameeso-iOS-SDK.zip so it wont get uploaded to GitHub Releases"
+	rm Gameeso-iOS-SDK.zip
