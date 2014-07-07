10c15e78a2a531a235a20a7e37c3c6fd70f21f99
Peter Wilemsen <peter@codebuffet.co>
2014-07-07 11:54:09 +0200

    Using travis conditiononal releasing now

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 xctool -project OpenKit.xcodeproj -scheme OpenKit
 python package_sdk.py
-
-if [[ $TRAVIS_BRANCH != 'master' ]]
-then
-	echo "Not on master branch, Deleting Gameeso-iOS-SDK.zip so it wont get uploaded to GitHub Releases"
-	rm Gameeso-iOS-SDK.zip
-fi
